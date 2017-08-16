7637c4ecf250549d57914a7bc87793ffc5a5c8fd
AAA48574 <aaronp261@gmail.com>
2017-08-16 19:16:48 +0100

    Moving the configuration class usable if this is used as a dep.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 "use strict"
 
 var LegacyMediaHubController = require('./src/legacy-media-hub-controller');
-
-class Configuration {
-    constructor() {
-        this.port = process.env.PORT;
-        this.hubUrl = process.env.HUB_URL;
-        this.hubPassword = process.env.HUB_PASSWORD;
-    }
-}
+var Configuration = require('./configuration');
 
 var hubController = new LegacyMediaHubController(new Configuration());
 hubController.init();
